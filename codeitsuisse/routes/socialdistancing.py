57b1e65e83b1983cefe84048a5d8f5474ee60b75
Siddhant Bagri <44798716+sidb28@users.noreply.github.com>
2020-09-26 17:22:10 +0800

    Social distancing (#7)
    * social distancing solution
    
    * Fix bugs in socialdistancing
    
    Co-authored-by: rhea20 <rhea.g2000@gmail.com>

---
+++ codeitsuisse/routes/socialdistancing.py
@@ -0,0 +1,30 @@
+import logging
+import json
+import math
+
+from flask import request, jsonify
+from math import comb
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/social_distancing', methods=['POST'])
+
+def social_distancing_evaluate():
+    data = request.get_json()
+    #logging.info("data sent for evaluation {}".format(data))
+    testVals = data.get("tests")
+    result = []
+    for i in range(len(testVals)):
+        x=testVals[str(i)]["seats"]
+        y=testVals[str(i)]["people"]
+        z=testVals[str(i)]["spaces"]
+        result.append(comb(x-(z*(y-1)), y))
+    
+    answer = {"answers": {}}
+    for p, val in enumerate(result):
+        answer["answers"][str(p)] = val
+    
+    #logging.info("My result :{}".format(result))
+    return json.dumps(answer)

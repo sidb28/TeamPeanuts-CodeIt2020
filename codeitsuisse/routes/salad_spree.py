b42cde3b77ecd9cc94b3d6cc1469c7d6c70fcd64
SidB <sid.bagri15@gmail.com>
2020-09-26 12:05:58 +0800

    Complete salad-spree

---
+++ codeitsuisse/routes/salad_spree.py
@@ -0,0 +1,30 @@
+import logging
+import json
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/salad-spree', methods=['POST'])
+def calculateprice():
+    data = request.get_json()
+    
+    n = data.get("number_of_salads")
+    streets = data.get("salad_prices_street_map")
+    costs = []
+    result = 0
+    
+    for street in streets:
+        for i in range(0,len(street)-n+1):
+            consecutive = street[i:i+n]
+            if "X" not in consecutive:
+                consecutive[:] = list(map(int, consecutive))
+                costs.append(sum(consecutive))
+    if costs:
+        result = min(costs)
+
+    returndict = {"result" : result}
+    return jsonify(returndict)    
+

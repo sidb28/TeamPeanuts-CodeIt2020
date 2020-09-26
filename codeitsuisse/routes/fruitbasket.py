8ebdf295f9d1d26ccd40dff090b963d73321da6e
Pranay Agarwal <pranay12@connect.hku.hk>
2020-09-26 14:34:05 +0800

    added weights

--- codeitsuisse/routes/fruitbasket.py
+++ codeitsuisse/routes/fruitbasket.py
@@ -9,6 +9,11 @@ logger = logging.getLogger(__name__)
 
 @app.route('/fruitbasket', methods=['POST'])
 def fruit():
+    weights = {
+        "maApple": 1,
+        "maWatermelon": 2,
+        "maAvocado": 3
+    }
     data = json.loads(request.data.decode("utf-8"))
     logging.info("data sent for evaluation {}".format(data))
     print(data)

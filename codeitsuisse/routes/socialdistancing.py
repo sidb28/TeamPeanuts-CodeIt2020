908ce9d91b7d1d9722d16223b09d11e2f4f73c1b
Pranay Agarwal <pranay12@connect.hku.hk>
2020-09-26 18:08:26 +0800

    main method fix + comb patch

--- codeitsuisse/routes/socialdistancing.py
+++ codeitsuisse/routes/socialdistancing.py
@@ -9,26 +9,27 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 def comb(n,k):
-    ans=1
-    for i in range(k):
-        ans = ans*(n-i)
-    return ans
+    numerator = 1
+    denom1, denom2 = 0
+    for i in range(1,n+1):
+        numerator*=i
+        if(i==k):
+            denom1 = numerator
+        if(i==n-k):
+            denom2 = numerator
+    return numerator/(denom1*denom2)
 
 @app.route('/social_distancing', methods=['POST'])
 def social_distancing_evaluate():
     data = request.get_json()
-    #logging.info("data sent for evaluation {}".format(data))
+    logging.info("data sent for evaluation {}".format(data))
     testVals = data.get("tests")
     result = []
-    for i in range(len(testVals)):
-        x=testVals[str(i)]["seats"]
-        y=testVals[str(i)]["people"]
-        z=testVals[str(i)]["spaces"]
-        result.append(comb(x-(z*(y-1)), y))
-    
     answer = {"answers": {}}
-    for p, val in enumerate(result):
-        answer["answers"][str(p)] = val
-    
-    #logging.info("My result :{}".format(result))
+    for key in testVals:
+        x=testVals[key]["seats"]
+        y=testVals[key]["people"]
+        z=testVals[key]["spaces"]
+        answer["answers"][key] = comb(x-(z*(y-1)), y)    
+    logging.info("My result :{}".format(result))
     return json.dumps(answer)

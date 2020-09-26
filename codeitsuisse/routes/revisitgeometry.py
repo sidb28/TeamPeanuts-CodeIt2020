f9681feaa62d40cf0cdb4a21ad0a6334631c5316
Pranay Agarwal <pranay12@connect.hku.hk>
2020-09-26 13:43:47 +0800

    single looped

--- codeitsuisse/routes/revisitgeometry.py
+++ codeitsuisse/routes/revisitgeometry.py
@@ -20,20 +20,20 @@ def intersection():
     Y1 = lineCoord[0]['y']
     X2 = lineCoord[1]['x']
     Y2 = lineCoord[1]['y']
-    for i in range(len(shapeCoord)-1):
-        for j in range(i,len(shapeCoord)):
-            X3 = shapeCoord[i]['x']
-            Y3 = shapeCoord[i]['y']
-            X4 = shapeCoord[j]['x']
-            Y4 = shapeCoord[j]['y']
+    for i in range(len(shapeCoord)):
+        j = 0 if i == len(shapeCoord) - 1 else i+1
+        X3 = shapeCoord[i]['x']
+        Y3 = shapeCoord[i]['y']
+        X4 = shapeCoord[j]['x']
+        Y4 = shapeCoord[j]['y']
 
-            if (X1-X2)*(Y3-Y4)-(Y1-Y2)*(X3-X4)==0:
-                continue
-            X = ((X1*Y2-Y1*X2)*(X3-X4)-(X1-X2)*(X3*Y4-Y3*X4)) / ((X1-X2)*(Y3-Y4)-(Y1-Y2)*(X3-X4))
-            Y = ((X1*Y2-Y1*X2)*(Y3-Y4)-(Y1-Y2)*(X3*Y4-Y3*X4)) / ((X1-X2)*(Y3-Y4)-(Y1-Y2)*(X3-X4))
-        
-            if (X-X3)*(X-X4) < 0 and (Y-Y3)*(Y-Y4) < 0:
-                result.add((X,Y))
+        if (X1-X2)*(Y3-Y4)-(Y1-Y2)*(X3-X4)==0:
+            continue
+        X = ((X1*Y2-Y1*X2)*(X3-X4)-(X1-X2)*(X3*Y4-Y3*X4)) / ((X1-X2)*(Y3-Y4)-(Y1-Y2)*(X3-X4))
+        Y = ((X1*Y2-Y1*X2)*(Y3-Y4)-(Y1-Y2)*(X3*Y4-Y3*X4)) / ((X1-X2)*(Y3-Y4)-(Y1-Y2)*(X3-X4))
+    
+        if (X-X3)*(X-X4) < 0 and (Y-Y3)*(Y-Y4) < 0:
+            result.add((X,Y))
         
     listOfCoordinates = []
     for coord in result:

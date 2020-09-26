2f0974ee5757403e4ec6cda176d7516277a7aab3
Pranay Agarwal <pranay12@connect.hku.hk>
2020-09-26 13:39:25 +0800

    list refactor + parallel check

--- codeitsuisse/routes/revisitgeometry.py
+++ codeitsuisse/routes/revisitgeometry.py
@@ -27,12 +27,19 @@ def intersection():
             X4 = shapeCoord[j]['x']
             Y4 = shapeCoord[j]['y']
 
+            if (X1-X2)*(Y3-Y4)-(Y1-Y2)*(X3-X4)==0:
+                continue
             X = ((X1*Y2-Y1*X2)*(X3-X4)-(X1-X2)*(X3*Y4-Y3*X4)) / ((X1-X2)*(Y3-Y4)-(Y1-Y2)*(X3-X4))
             Y = ((X1*Y2-Y1*X2)*(Y3-Y4)-(Y1-Y2)*(X3*Y4-Y3*X4)) / ((X1-X2)*(Y3-Y4)-(Y1-Y2)*(X3-X4))
         
             if (X-X3)*(X-X4) < 0 and (Y-Y3)*(Y-Y4) < 0:
                 result.add((X,Y))
         
-    
-   # logging.info("My result :{}".format(result))
-    return json.dumps(result)
+    listOfCoordinates = []
+    for coord in result:
+        listOfCoordinates.append(jsonify({
+            "x": coord[0],
+            "y": coord[1]
+        }))
+   # logging.info("My result :{}".format(listOfCoordinates))
+    return json.dumps(listOfCoordinates)

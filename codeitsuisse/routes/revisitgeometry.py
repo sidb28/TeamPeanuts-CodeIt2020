143f6ae12ff0648dd1b6ed668bfe10650dcbb998
rhea20 <rhea.g2000@gmail.com>
2020-09-26 13:22:48 +0800

    revisitGeom changes

--- codeitsuisse/routes/revisitgeometry.py
+++ codeitsuisse/routes/revisitgeometry.py
@@ -20,17 +20,19 @@ def intersection():
     Y1 = lineCoord[0]['y']
     X2 = lineCoord[1]['x']
     Y2 = lineCoord[1]['y']
-    for i in range(shapeCoord):
-        if i == len(shapeCoord)-1:
-            break
-        X3 = shapeCoord[i]['x']
-        Y3 = shapeCoord[i]['y']
-        X4 = shapeCoord[i+1]['x']
-        Y4 = shapeCoord[i+1]['y']
+    for i in range(shapeCoord)-2:
+        for j in range(i,shapeCoord):
+            X3 = shapeCoord[i]['x']
+            Y3 = shapeCoord[i]['y']
+            X4 = shapeCoord[j]['x']
+            Y4 = shapeCoord[j]['y']
 
-        X = ((X1*Y2-Y1*X2)*(X3-X4)-(X1-X2)*(X3*Y4-Y3*X4)) / ((X1-X2)*(Y3-Y4)-(Y1-Y2)*(X3-X4))
-        Y = ((X1*Y2-Y1*X2)*(Y3-Y4)-(Y1-Y2)*(X3*Y4-Y3*X4)) / ((X1-X2)*(Y3-Y4)-(Y1-Y2)*(X3-X4))
-        result.add((X,Y))
+            X = ((X1*Y2-Y1*X2)*(X3-X4)-(X1-X2)*(X3*Y4-Y3*X4)) / ((X1-X2)*(Y3-Y4)-(Y1-Y2)*(X3-X4))
+            Y = ((X1*Y2-Y1*X2)*(Y3-Y4)-(Y1-Y2)*(X3*Y4-Y3*X4)) / ((X1-X2)*(Y3-Y4)-(Y1-Y2)*(X3-X4))
+        
+            if (X-X3)*(X-X4) < 0 and (Y-Y3)*(Y-Y4) < 0:
+                result.add((X,Y))
+        
     
    # logging.info("My result :{}".format(result))
     return json.dumps(result)

7a12a3462cb3d82b58e03f0562db38618e65d6ef
Rhea Gupta <47170186+rhea20@users.noreply.github.com>
2020-09-26 14:02:37 +0800

    Revisit geometry (#6)
    * revisitGeom check
    
    * revisitGeom changes
    
    * init.py modified
    
    * loop changes
    
    * list refactor + parallel check
    
    * single looped
    
    * single looped
    
    * line checking condition fix
    
    Co-authored-by: Pranay Agarwal <pranay12@connect.hku.hk>
    Co-authored-by: Siddhant Bagri <44798716+sidb28@users.noreply.github.com>

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,7 +1,9 @@
-from flask import Flask;
+from flask import Flask
 app = Flask(__name__)
 import codeitsuisse.routes.square
+import codeitsuisse.routes.revisitgeometry
 import codeitsuisse.routes.salad_spree
+
 
 
 

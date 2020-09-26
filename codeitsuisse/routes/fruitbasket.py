ef7847743dabdf410eaf3acc4628350617462212
Pranay Agarwal <pranay12@connect.hku.hk>
2020-09-26 14:58:44 +0800

    added weights and store

--- codeitsuisse/routes/fruitbasket.py
+++ codeitsuisse/routes/fruitbasket.py
@@ -10,14 +10,23 @@ logger = logging.getLogger(__name__)
 @app.route('/fruitbasket', methods=['POST'])
 def fruit():
     weights = {
-        "maApple": 1,
-        "maWatermelon": 2,
-        "maAvocado": 3
+        "maApple": 50,
+        "maWatermelon": 50,
+        "maAvocado": 50
     }
     data = json.loads(request.data.decode("utf-8"))
     logging.info("data sent for evaluation {}".format(data))
-    print(data)
     result = 0
+    for key in data:
+        weightOfFruit = 0
+        try:
+            weightOfFruit = weights[key]
+        except:
+            print(key)
+            weightOfFruit = 1
+        finally:
+            result += data[key]*weightOfFruit
+
     logging.info("My result :{}".format(result))
     return json.dumps(result)
 

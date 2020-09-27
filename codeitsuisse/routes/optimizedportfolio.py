8c1fa96118d6156cff4c2202f375e65c8502b10d
rhea20 <rhea.g2000@gmail.com>
2020-09-27 08:50:13 +0800

    Added new loop

--- codeitsuisse/routes/optimizedportfolio.py
+++ codeitsuisse/routes/optimizedportfolio.py
@@ -19,64 +19,67 @@ def portfolio_evaluate():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     inputVal = data.get("inputs")
-    inp = inputVal[0]
 
     ohr = {}
     numfut = {}
-    
-    portfolioVal = inp["Portfolio"]["Value"]
-    SpotVol = inp["Portfolio"]["SpotPrcVol"]
+    result = {"outputs": []}
 
-    minOHR = 0
-    minfutVol = inp["IndexFutures"][0]["FuturePrcVol"]
-    minfutVolkey = inp["IndexFutures"][0]["Name"]
-    minOHRkey = inp["IndexFutures"][0]["Name"]
-    minNumFutkey = inp["IndexFutures"][0]["Name"]
+    for i in range(len(inputVal)):
+        inp = inputVal[i]
+        portfolioVal = inp["Portfolio"]["Value"]
+        SpotVol = inp["Portfolio"]["SpotPrcVol"]
 
-    for j in range(len(inp["IndexFutures"])):
-        temp = inp["IndexFutures"][j]
-
-        name = temp["Name"]
-        cr = temp["CoRelationCoefficient"]
-
-        t1 = temp["FuturePrcVol"]
-        
-        if t1 < minfutVol :
-            minfutVol = t1
-            minfutVolkey = name
-        
-        futPrice = temp["IndexFuturePrice"]
-        notional = temp["Notional"]
-
-        t2 = calcOHR(cr, SpotVol, t1)
-        if j == 0:
-            minOHR = round(t2,3)
-        elif t2 < minOHR:
-            minOHR = round(t2,3)
-            minOHRkey = name
-        ohr[name] = round(t2,3)
-
-        t3 = futCont(ohr[name], portfolioVal, futPrice, notional)
-        if j == 0:
-            minNumFut = round(t3)
-        elif t3 < minOHR:
-            minNumFut = round(t3)
-            minNumFutkey = name
-        numfut[name]=round(t3)
-
-    if minOHRkey == minfutVolkey:
-        result = {"outputs": [{
-            "HedgePositionName" : minOHRkey, 
-            "OptimalHedgeRatio" : minOHR, 
-            "NumFuturesContract" : numfut[minOHRkey]
-            }]}
+        for j in range(len(inp["IndexFutures"])):
             
-    else:
-        result = {"outputs": [{
-            "HedgePositionName" : minNumFutkey, 
-            "OptimalHedgeRatio" : ohr[minNumFutkey], 
-            "NumFuturesContract" : minNumFut
-            }]}
+            minOHR = 0
+            minfutVol = inp["IndexFutures"][0]["FuturePrcVol"]
+            minfutVolkey = inp["IndexFutures"][0]["Name"]
+            minOHRkey = inp["IndexFutures"][0]["Name"]
+            minNumFutkey = inp["IndexFutures"][0]["Name"]
+            temp = inp["IndexFutures"][j]
+
+            name = temp["Name"]
+            cr = temp["CoRelationCoefficient"]
+
+            t1 = temp["FuturePrcVol"]
+        
+            if t1 < minfutVol :
+                minfutVol = t1
+                minfutVolkey = name
+        
+            futPrice = temp["IndexFuturePrice"]
+            notional = temp["Notional"]
+
+            t2 = calcOHR(cr, SpotVol, t1)
+            if j == 0:
+                minOHR = round(t2,3)
+            elif t2 < minOHR:
+                minOHR = round(t2,3)
+                minOHRkey = name
+            ohr[name] = round(t2,3)
+
+            t3 = futCont(ohr[name], portfolioVal, futPrice, notional)
+            if j == 0:
+                minNumFut = round(t3)
+            elif t3 < minOHR:
+                minNumFut = round(t3)
+                minNumFutkey = name
+            numfut[name]=round(t3)
+
+        if minOHRkey == minfutVolkey:
+            result["outputs"].append({
+                "HedgePositionName" : minOHRkey, 
+                "OptimalHedgeRatio" : minOHR, 
+                "NumFuturesContract" : numfut[minOHRkey]
+            })
+            
+            
+        else:
+            result["outputs"].append({
+                "HedgePositionName" : minNumFutkey, 
+                "OptimalHedgeRatio" : ohr[minNumFutkey], 
+                "NumFuturesContract" : minNumFut
+                })
        
     logging.info("My result :{}".format(result))
     return json.dumps(result)
